2cc5b3b8567ae023c33d2ea87a8c7f91d6dfce93
4nswer <34008203+4nswer@users.noreply.github.com>
2020-12-26 19:51:01 +0000

    Changes to SSI_Shift_In and SSI_Shift_Out functions

--- AEAT-6600-T16.ino
+++ AEAT-6600-T16.ino
@@ -17,10 +17,10 @@ const int MAG_HI = 8,
             ALIGN = 2,
             PWR_DN = 3,
             PROG = 4,
-            NCS = 5;
-const int CLOCK_PIN = 6; //The clock pin is also accessed directly as PORTD{7} in the readPosition() function. Changing the code only here will not work.
-const int DATA_PIN = 7; //The data pin is also accessed directly as PINE{6} in the readPosition() function. Changing the code only here will not work.
-const int BIT_COUNT = 10; // this's the precision of rotary encoder.
+            NCS = 5,
+            CLOCK_PIN = 6, //The clock pin is also accessed directly as PORTD{7} in the readPosition() function. Changing the code only here will not work.
+            DATA_PIN = 7, //The data pin is also accessed directly as PINE{6} in the readPosition() function. Changing the code only here will not work.
+            BIT_COUNT = 10; // this's the precision of rotary encoder.
 
 void setup() {
   pinMode(MAG_HI, INPUT);
@@ -38,11 +38,12 @@ void setup() {
 
 void loop() {
   
+  if (Serial.available())
+  {
   switch (Serial.read())
   {
   case 'a':
     while (Serial.read()!='e')
-    for(int i = 0; i<10; i++)
     {
       Serial.println(readPosition(),2);
       delay(500);
@@ -86,11 +87,14 @@ void loop() {
     break;
 
     case 'd':
-    SSI_Shift_Out(32,0b00000000000000000000000000000000);
+    int32_t out1= 0b00000000000000000100101100000111;
+    Serial.print(out1);
+    SSI_Shift_Out(32,out1);
     break;
 
   default:
     break;
+  }
   }
 }
 
@@ -102,31 +106,47 @@ float readPosition() {
 }
 
 //read in a byte of data from the digital input of the board.
-unsigned long SSI_Shift_In(const int data_pin, const int clock_pin, const int bit_count) {
+unsigned long SSI_Shift_In(const int data_pin, const int clock_pin, const int bit_count)
+{
   unsigned long data = 0;
-
+  // First tick is to initialise the transfer, no data is read.
+  PORTD &= ~(1 << 7); // clock pin low
+  delayMicroseconds(2);
+  PORTD |= (1 << 7); // clock pin high
+  delayMicroseconds(2);
   for (int i=0; i<bit_count; i++) {
     data <<= 1; // shift all read data left one bit.
     PORTD &= ~(1 << 7); // clock pin low
     delayMicroseconds(2);
     PORTD |= (1 << 7); // clock pin high
-    delayMicroseconds(1); // This is shorter than for the low time to account for the time read the port below. 
-    data |= (bitRead(PINE,6)); // append the new read bit to the whole read data.
-    /*bitRead(port,bit) is defined in arduino.h and takes approximately 1us.
+    delayMicroseconds(1); // This is shorter than for the low time to account for the time read the port below. As it stands the time taken to read the data is about 1.5us so the delay is mostly for stability.
+    data |= (bitRead(PINE,6)); // Append the new read bit to data.
+    /* bitRead(port,bit) is defined in arduino.h and takes approximately 1us.
      * data |= digitalRead(data_pin); <- takes about 4us and limits the SSI rate to less than 100kHz. It is also increases the jitter.
      */
-  }
+    }
   return data;
 }
 
 void SSI_Shift_Out(const int bit_count, int progData) {
   pinMode(DATA_PIN, OUTPUT);
-  for (int i=0; i<bit_count; i++) {
+  for (int i=0; i<bit_count+2; i++) {
     PORTD &= ~(1 << 7); // clock pin low
-    delayMicroseconds(2);
+    delayMicroseconds(4);
     PORTD |= (1 << 7); // clock pin high
-    delayMicroseconds(1); // This is shorter than for the low time to account for the time read the port below. 
-    bitWrite (PORTE,i, bitRead(progData,i));
+    delayMicroseconds(1); // This is shorter than for the low time to account for the time write the port below.
+    if (i==0)
+    {
+      bitWrite (PORTE,6,1);
+    }
+    if (i==bit_count+2)
+    {
+      bitWrite (PORTE,6,1);
+    }
+    if (i>0&&i<bit_count+2)
+    {
+      bitWrite (PORTE,6, bitRead(progData,i));
+    }
     //data |= (bitRead(PINE,6)); // append the new read bit to the whole read data.
     /*bitRead(port,bit) is defined in arduino.h and takes approximately 1us.
      * data |= digitalRead(data_pin); <- takes about 4us and limits the SSI rate to 100kHz. It is also increases the jitter.
@@ -134,3 +154,5 @@ void SSI_Shift_Out(const int bit_count, int progData) {
   }
   pinMode(DATA_PIN, INPUT);
 }
+
+
