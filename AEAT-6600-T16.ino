e4dcd41adee2b433eaaaa2e6f025d7183c6127d9
4nswer <34008203+4nswer@users.noreply.github.com>
2021-05-10 22:17:27 +0100

    Working on parameter writing

--- AEAT-6600-T16.ino
+++ AEAT-6600-T16.ino
@@ -37,64 +37,63 @@ void setup() {
 }
 
 void loop() {
-  
   if (Serial.available())
   {
-  switch (Serial.read())
-  {
-  case 'a':
-    while (Serial.read()!='e')
+    switch (Serial.read())
     {
-      Serial.println(readPosition(),2);
-      delay(500);
-    }
-    break;
-
-  case 'b':
-    while (Serial.read()!='e')
-    {
-      if (digitalRead(MAG_HI))
-      {
-        Serial.println("Magnetic field intensity is too high");
-      }
-      if (digitalRead(MAG_LO))
-      {
-        Serial.println("Magnetic field intensity is too low");
-      }
-      if (!digitalRead(MAG_HI)&!digitalRead(MAG_LO))
-      {
-        Serial.print("Magnetic field intensity is correct. ");
-        Serial.print("Position Reading is: ");
-        Serial.println(readPosition(),2);
-      }
-      if (digitalRead(MAG_HI)&digitalRead(MAG_LO))
-      {
-        Serial.println("Fault, both pins reading high which should not be possible");
-      }
-      delay(200);
-    }
-    break;
-
-    case 'c':
+    case 'a':
       while (Serial.read()!='e')
       {
-        digitalWrite(ALIGN,HIGH);
-        Serial.print("The alignment value is: ");
-        Serial.println(SSI_Shift_In(DATA_PIN, CLOCK_PIN, 16));
-        delay(100);
+        Serial.println(readPosition(),2);
+        delay(10);
       }
-      digitalWrite(ALIGN,LOW);
-    break;
+      break;
 
-    case 'd':
-    int32_t out1= 0b00000000000000000100101100000111;
-    Serial.print(out1);
-    SSI_Shift_Out(32,out1);
-    break;
+    case 'b':
+      while (Serial.read()!='e')
+      {
+        if (digitalRead(MAG_HI))
+        {
+          Serial.println("Magnetic field intensity is too high");
+        }
+        if (digitalRead(MAG_LO))
+        {
+          Serial.println("Magnetic field intensity is too low");
+        }
+        if (!digitalRead(MAG_HI)&!digitalRead(MAG_LO))
+        {
+          Serial.print("Magnetic field intensity is correct. ");
+          Serial.print("Position Reading is: ");
+          Serial.println(readPosition(),2);
+        }
+        if (digitalRead(MAG_HI)&digitalRead(MAG_LO))
+        {
+          Serial.println("Fault, both pins reading high which should not be possible");
+        }
+        delay(200);
+      }
+      break;
 
-  default:
-    break;
-  }
+      case 'c':
+        while (Serial.read()!='e')
+        {
+          digitalWrite(ALIGN,HIGH);
+          Serial.print("The alignment value is: ");
+          Serial.println(SSI_Shift_In(DATA_PIN, CLOCK_PIN, 16));
+          delay(100);
+        }
+        digitalWrite(ALIGN,LOW);
+      break;
+
+      case 'd':
+      int32_t out1= 0b00000000000000000100101100000111;
+      Serial.print(out1);
+      SSI_Shift_Out(32,out1);
+      break;
+
+    default:
+      break;
+    }
   }
 }
 
@@ -130,11 +129,13 @@ unsigned long SSI_Shift_In(const int data_pin, const int clock_pin, const int bi
 
 void SSI_Shift_Out(const int bit_count, int progData) {
   pinMode(DATA_PIN, OUTPUT);
+  PORTD &= ~(1 << 7); // clock pin low
+  bitWrite (PORTE,6,1);
+  delayMicroseconds(1);
+  PORTD |= (1 << 7); // clock pin high
   for (int i=0; i<bit_count+2; i++) {
     PORTD &= ~(1 << 7); // clock pin low
-    delayMicroseconds(4);
-    PORTD |= (1 << 7); // clock pin high
-    delayMicroseconds(1); // This is shorter than for the low time to account for the time write the port below.
+    delayMicroseconds(1);
     if (i==0)
     {
       bitWrite (PORTE,6,1);
@@ -143,10 +144,12 @@ void SSI_Shift_Out(const int bit_count, int progData) {
     {
       bitWrite (PORTE,6,1);
     }
-    if (i>0&&i<bit_count+2)
+    if ((i > 0) && i<bit_count+2)
     {
       bitWrite (PORTE,6, bitRead(progData,i));
     }
+    PORTD |= (1 << 7); // clock pin high
+    delayMicroseconds(2); // This is shorter than for the low time to account for the time write the port below.
     //data |= (bitRead(PINE,6)); // append the new read bit to the whole read data.
     /*bitRead(port,bit) is defined in arduino.h and takes approximately 1us.
      * data |= digitalRead(data_pin); <- takes about 4us and limits the SSI rate to 100kHz. It is also increases the jitter.
